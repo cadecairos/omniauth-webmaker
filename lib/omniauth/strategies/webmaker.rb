552b148df33a39e467d5acf007f8891d5995e26d
Christopher De Cairos <cade@mozillafoundation.org>
2014-03-14 15:42:38 -0400

    Error handling

--- lib/omniauth/strategies/webmaker.rb
+++ lib/omniauth/strategies/webmaker.rb
@@ -8,14 +8,32 @@ module OmniAuth
     class Webmaker
       include OmniAuth::Strategy
 
+      class WebmakerError < StandardError
+        attr_accessor :error
+
+        def initialize(error)
+          self.error = error
+        end
+
+        def message
+          error
+        end
+      end
+
       option :login_server_url, "http://localhost:3000/api/user/authenticate"
       option :name, "webmaker"
       option :client_options, {}
 
       info do
         {
-          :email => verify_webmaker['user']['email']
+          :email => verify_webmaker
         }
+      end
+
+      def callback_phase
+        super
+      rescue WebmakerError => e
+        fail! e.message, e
       end
 
       def verify_webmaker
@@ -26,7 +44,17 @@ module OmniAuth
           :audience => req_body["audience"]
         )
 
-        resp = MultiJson.decode(response.body)
+        resp_body = MultiJson.decode(response.body)
+
+        if resp_body.has_key? "user"
+          resp_body["user"]["email"]
+        elsif resp_body.has_key? "error"
+          raise WebmakerError, "Error Logging In"
+        elsif resp_body.has_key? "email"
+          raise WebmakerError, "You Must <a href=\\\"https://webmaker.org\\\"> create a Webmaker account</a> to log in"
+        else
+          raise WebmakerError, "Unknown Error"
+        end
       end
     end
   end
